838a1befd4bba49cc953654cab815c550615f0a0
FredNekrasov <frednekrasov49@gmail.com>
2024-03-08 16:36:15 +0300

    очистка класса MainActivity.kt

--- app/src/main/java/com/alexander_nevsky_temple/MainActivity.kt
+++ app/src/main/java/com/alexander_nevsky_temple/MainActivity.kt
@@ -6,39 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.alexander_nevsky_temple.ui.theme.AlexanderNevskyTempleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AlexanderNevskyTempleTheme { // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            AlexanderNevskyTempleTheme {
+                Surface(Modifier.fillMaxSize(),color = MaterialTheme.colorScheme.background) {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String,modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlexanderNevskyTempleTheme {
-        Greeting("Android")
     }
 }
