103676295bf45206761a9008b6abe2fefaa85439
FredNekrasov <frednekrasov49@gmail.com>
2024-03-08 21:15:53 +0300

    Создание констант с наименованием заголовка

---
+++ app/src/main/java/com/alexander_nevsky_temple/ui/navigation/TitleStrings.kt
@@ -0,0 +1,16 @@
+package com.alexander_nevsky_temple.ui.navigation
+
+object TitleStrings {
+    const val MAIN_TITLE = "Храм благоверного князя Александра Невского"
+    const val MAIN = "Главная"
+    const val LIFE = "Приходская жизнь"
+    const val SCHEDULE = "Расписание Богослужений"
+    const val BLESSING = "Духовные беседы"
+    const val CLUB = "Молодежный клуб"
+    const val SORCERY = "Священство"
+    const val PRIESTS = "Советы священника"
+    const val HISTORY = "История"
+    const val CHANTS = "Требы"
+    const val CONTACTS = "Контакты"
+    const val INFORMATION = "Информация"
+}
