dec1855db7f9311fc4906b04ca78db7841a564f6
FredNekrasov <frednekrasov49@gmail.com>
2024-03-17 16:45:30 +0300

    переименование данных в ScreenRoute.kt и TitleStrings.kt

--- app/src/main/java/com/alexander_nevsky_temple/ui/navigation/TitleStrings.kt
+++ app/src/main/java/com/alexander_nevsky_temple/ui/navigation/TitleStrings.kt
@@ -1,13 +1,13 @@
 package com.alexander_nevsky_temple.ui.navigation
 
 object TitleStrings {
-    const val MAIN_TITLE = "Храм благоверного князя Александра Невского"
+    const val MAIN_TITLE = "Храм Александра Невского"
     const val MAIN = "Главная"
     const val LIFE = "Приходская жизнь"
     const val SCHEDULE = "Расписание Богослужений"
-    const val BLESSING = "Духовные беседы"
+    const val CONVERSATION = "Духовные беседы"
     const val CLUB = "Молодежный клуб"
-    const val SORCERY = "Священство"
+    const val PRIESTHOOD = "Священство"
     const val PRIESTS = "Советы священника"
     const val HISTORY = "История"
     const val CHANTS = "Требы"
