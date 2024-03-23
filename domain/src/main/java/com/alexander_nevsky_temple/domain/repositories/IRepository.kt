02e15096184fedf72783e64107aced09b69b5f69
FredNekrasov <frednekrasov49@gmail.com>
2024-03-23 08:46:55 +0300

    refactoring an interface for repositories in the domain layer

--- domain/src/main/java/com/alexander_nevsky_temple/domain/repositories/IRepository.kt
+++ domain/src/main/java/com/alexander_nevsky_temple/domain/repositories/IRepository.kt
@@ -3,6 +3,6 @@ package com.alexander_nevsky_temple.domain.repositories
 import com.alexander_nevsky_temple.domain.utils.ConnectionStatus
 import kotlinx.coroutines.flow.StateFlow
 
-interface IRepository {
-    suspend fun getList(): StateFlow<ConnectionStatus>
+interface IRepository<M> {
+    suspend fun getList() : StateFlow<ConnectionStatus<M>>
 }
