f2cd754a4ae7dca56da2c97285c4967eab748bd9
FredNekrasov <frednekrasov49@gmail.com>
2024-03-23 12:25:22 +0300

    creating a function that creates a Retrofit instance that implements an IArticleService interface for network requests

--- data/src/main/java/com/alexander_nevsky_temple/data/remote/services/IArticleService.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/remote/services/IArticleService.kt
@@ -1,9 +1,18 @@
 package com.alexander_nevsky_temple.data.remote.services
 
 import com.alexander_nevsky_temple.data.remote.dto.ArticleDto
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface IArticleService {
     @GET("/api/Article")
     suspend fun getDtoList(): List<ArticleDto>?
+    companion object {
+        fun getService(url: String) : IArticleService = Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(IArticleService::class.java)
+    }
 }
