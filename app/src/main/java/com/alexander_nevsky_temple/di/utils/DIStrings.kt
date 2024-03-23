fab9510899f0adbb5a9022399f8a6472a492e173
FredNekrasov <frednekrasov49@gmail.com>
2024-03-23 11:41:34 +0300

    creating an object containing data for Koin DI qualifiers

---
+++ app/src/main/java/com/alexander_nevsky_temple/di/utils/DIStrings.kt
@@ -0,0 +1,11 @@
+package com.alexander_nevsky_temple.di.utils
+
+object DIStrings {
+    const val SERVICE = "service"
+    const val REPOSITORY = "repository"
+    const val USE_CASE = "useCase"
+    const val VIEWMODEL = "viewmodel"
+
+    const val ARTICLE = "article"
+    const val ARTICLE_TYPE = "articleType"
+}
