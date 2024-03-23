77d6536109bd66836502d91e3d1624f180a7ed9c
FredNekrasov <frednekrasov49@gmail.com>
2024-03-23 12:20:10 +0300

    adding a base URL to DIStrings.kt

--- app/src/main/java/com/alexander_nevsky_temple/di/utils/DIStrings.kt
+++ app/src/main/java/com/alexander_nevsky_temple/di/utils/DIStrings.kt
@@ -1,6 +1,8 @@
 package com.alexander_nevsky_temple.di.utils
 
 object DIStrings {
+    const val BASE_URL = "http://localhost:port"
+
     const val SERVICE = "service"
     const val REPOSITORY = "repository"
     const val USE_CASE = "useCase"
