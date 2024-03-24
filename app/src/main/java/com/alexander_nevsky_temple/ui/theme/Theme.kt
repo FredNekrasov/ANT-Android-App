5ce138afa80dfa109b96252b3e4b3687de5010dc
FredNekrasov <frednekrasov49@gmail.com>
2024-03-24 11:28:20 +0300

    editing the Theme.kt file

--- app/src/main/java/com/alexander_nevsky_temple/ui/theme/Theme.kt
+++ app/src/main/java/com/alexander_nevsky_temple/ui/theme/Theme.kt
@@ -15,23 +15,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,secondary = PurpleGrey80,tertiary = Pink80
-)
+private val DarkColorScheme = darkColorScheme(primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,secondary = PurpleGrey40,tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+private val LightColorScheme = lightColorScheme(primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40)
 
 @Composable
 fun AlexanderNevskyTempleTheme(
@@ -43,7 +29,6 @@ fun AlexanderNevskyTempleTheme(
             val context = LocalContext.current
             if(darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
@@ -56,7 +41,5 @@ fun AlexanderNevskyTempleTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,typography = Typography,content = content
-    )
+    MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
 }
