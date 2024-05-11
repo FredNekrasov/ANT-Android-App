1c59ca8612636f41237a507d46af72d7c62f4193
FredNekrasov <frednekrasov49@gmail.com>
2024-05-11 17:43:37 +0300

    refactoring the ConnectionStatus class

--- domain/src/main/java/com/alexander_nevsky_temple/domain/utils/ConnectionStatus.kt
+++ domain/src/main/java/com/alexander_nevsky_temple/domain/utils/ConnectionStatus.kt
@@ -1,11 +1,5 @@
 package com.alexander_nevsky_temple.domain.utils
 
-sealed class ConnectionStatus<M>(val list: List<M>, val message: String = "") {
-    class Loading<M>(list: List<M>) : ConnectionStatus<M>(list)
-    class Success<M>(list: List<M>) : ConnectionStatus<M>(list)
-    class ConnectionError<M>(message: String, list : List<M>) : ConnectionStatus<M>(list, message)
-    class NoInternet<M>(message: String, list: List<M>) : ConnectionStatus<M>(list, message)
-    class NoData<M>(message: String, list: List<M>) : ConnectionStatus<M>(list, message)
-    class SerializationError<M>(message: String, list: List<M>) : ConnectionStatus<M>(list, message)
-    class Unknown<M>(message: String, list: List<M>) : ConnectionStatus<M>(list, message)
+enum class ConnectionStatus {
+    LOADING, SUCCESS, CONNECTION_ERROR, NO_INTERNET, NO_DATA, SERIALIZATION_ERROR, UNKNOWN
 }
