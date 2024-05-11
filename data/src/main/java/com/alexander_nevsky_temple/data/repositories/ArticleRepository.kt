bfbb2044967bdb2f3b6ccb0a8b2c444fcde1554b
FredNekrasov <frednekrasov49@gmail.com>
2024-05-11 19:05:58 +0300

    refactoring repositories

--- data/src/main/java/com/alexander_nevsky_temple/data/repositories/ArticleRepository.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/repositories/ArticleRepository.kt
@@ -4,7 +4,8 @@ import com.alexander_nevsky_temple.data.mappers.toModel
 import com.alexander_nevsky_temple.data.remote.services.IArticleService
 import com.alexander_nevsky_temple.domain.model.Article
 import com.alexander_nevsky_temple.domain.repositories.IRepository
-import com.alexander_nevsky_temple.domain.utils.ConnectionStatus
+import com.alexander_nevsky_temple.domain.utils.*
+import com.alexander_nevsky_temple.domain.utils.ActionStatus.*
 import com.alexander_nevsky_temple.domain.utils.ConnectionStatus.*
 import com.google.gson.JsonSyntaxException
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,21 +16,21 @@ import java.io.IOException
 class ArticleRepository(
     private val service : IArticleService
 ) : IRepository<Article> {
-    override suspend fun getList() : StateFlow<ConnectionStatus<Article>> {
-        val data = MutableStateFlow<ConnectionStatus<Article>>(Loading(emptyList()))
+    override suspend fun getList() : StateFlow<ActionStatus<Article>> {
+        val data = MutableStateFlow<ActionStatus<Article>>(Loading(emptyList()))
         try {
-            val list = service.getDtoList() ?: throw NullPointerException("No data")
+            val list = service.getArticleList() ?: throw NullPointerException()
             data.emit(Success(list.map { it.toModel() }))
         } catch (e: NullPointerException) {
-            data.emit(NoData(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), NO_DATA))
         } catch (e: HttpException) {
-            data.emit(ConnectionError(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), CONNECTION_ERROR))
         } catch (e: IOException) {
-            data.emit(NoInternet(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), NO_INTERNET))
         } catch (e: JsonSyntaxException) {
-            data.emit(SerializationError(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), SERIALIZATION_ERROR))
         } catch (e: Exception) {
-            data.emit(Unknown(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), UNKNOWN))
         }
         return data
     }
--- data/src/main/java/com/alexander_nevsky_temple/data/repositories/ArticleTypeRepository.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/repositories/ArticleTypeRepository.kt
@@ -4,7 +4,8 @@ import com.alexander_nevsky_temple.data.mappers.toModel
 import com.alexander_nevsky_temple.data.remote.services.IArticleTypeService
 import com.alexander_nevsky_temple.domain.model.ArticleType
 import com.alexander_nevsky_temple.domain.repositories.IRepository
-import com.alexander_nevsky_temple.domain.utils.ConnectionStatus
+import com.alexander_nevsky_temple.domain.utils.ActionStatus.*
+import com.alexander_nevsky_temple.domain.utils.*
 import com.alexander_nevsky_temple.domain.utils.ConnectionStatus.*
 import com.google.gson.JsonSyntaxException
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,21 +16,21 @@ import java.io.IOException
 class ArticleTypeRepository(
     private val service: IArticleTypeService
 ) : IRepository<ArticleType> {
-    override suspend fun getList() : StateFlow<ConnectionStatus<ArticleType>> {
-        val data = MutableStateFlow<ConnectionStatus<ArticleType>>(Loading(emptyList()))
+    override suspend fun getList() : StateFlow<ActionStatus<ArticleType>> {
+        val data = MutableStateFlow<ActionStatus<ArticleType>>(Loading(emptyList()))
         try {
-            val list = service.getDtoList() ?: throw NullPointerException("No data")
+            val list = service.getArticleTypeList() ?: throw NullPointerException()
             data.emit(Success(list.map { it.toModel() }))
         } catch (e: NullPointerException) {
-            data.emit(NoData(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), NO_DATA))
         } catch (e: HttpException) {
-            data.emit(ConnectionError(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), CONNECTION_ERROR))
         } catch (e: IOException) {
-            data.emit(NoInternet(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), NO_INTERNET))
         } catch (e: JsonSyntaxException) {
-            data.emit(SerializationError(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), SERIALIZATION_ERROR))
         } catch (e: Exception) {
-            data.emit(Unknown(e.message.toString(), emptyList()))
+            data.emit(Error(emptyList(), UNKNOWN))
         }
         return data
     }
