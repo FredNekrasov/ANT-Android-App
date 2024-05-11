d7c891239cd270b60f56aec68f8e9f2607f7d42e
FredNekrasov <frednekrasov49@gmail.com>
2024-05-11 19:30:49 +0300

    refactoring mappers

--- data/src/main/java/com/alexander_nevsky_temple/data/mappers/ArticleTypeMapper.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/mappers/ArticleTypeMapper.kt
@@ -3,4 +3,4 @@ package com.alexander_nevsky_temple.data.mappers
 import com.alexander_nevsky_temple.data.remote.dto.ArticleTypeDto
 import com.alexander_nevsky_temple.domain.model.ArticleType
 
-fun ArticleTypeDto.toModel() = ArticleType(type, id)
+fun ArticleTypeDto.toModel() = ArticleType(name, id)
