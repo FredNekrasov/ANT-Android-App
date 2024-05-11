c41dbcc76e2b031a88ec0ccb36e850e21e8dc8b7
FredNekrasov <frednekrasov49@gmail.com>
2024-05-11 17:47:18 +0300

    removing unnecessary functions from the IArticleTypeService and IArticleService interfaces

--- data/src/main/java/com/alexander_nevsky_temple/data/remote/services/IArticleService.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/remote/services/IArticleService.kt
@@ -1,18 +1,12 @@
 package com.alexander_nevsky_temple.data.remote.services
 
 import com.alexander_nevsky_temple.data.remote.dto.ArticleDto
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.alexander_nevsky_temple.data.remote.dto.ContentDto
 import retrofit2.http.GET
 
 interface IArticleService {
     @GET("/api/Article")
-    suspend fun getDtoList(): List<ArticleDto>?
-    companion object {
-        fun getService(url: String) : IArticleService = Retrofit.Builder()
-            .baseUrl(url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(IArticleService::class.java)
-    }
+    suspend fun getArticleList(): List<ArticleDto>?
+    @GET("/api/Content")
+    suspend fun getContentList(): List<ContentDto>?
 }
