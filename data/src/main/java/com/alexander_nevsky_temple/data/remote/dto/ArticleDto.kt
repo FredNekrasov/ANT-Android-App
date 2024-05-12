a11c62a213360ba0bfe518098675ac3834a8d770
FredNekrasov <frednekrasov49@gmail.com>
2024-05-12 11:11:03 +0300

    editing the ArticleDto

--- data/src/main/java/com/alexander_nevsky_temple/data/remote/dto/ArticleDto.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/remote/dto/ArticleDto.kt
@@ -1,7 +1,7 @@
 package com.alexander_nevsky_temple.data.remote.dto
 
 data class ArticleDto(
-    val id: String,
+    val id: Long,
     val title: String,
     val description: String,
     val dateOrBanner: String,
