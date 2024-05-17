6294357bf35c626e3222fcb5ce56084a46213ce5
FredNekrasov <frednekrasov49@gmail.com>
2024-05-17 15:58:48 +0300

    refactoring DTOs

--- data/src/main/java/com/alexander_nevsky_temple/data/remote/dto/ArticleDto.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/remote/dto/ArticleDto.kt
@@ -1,9 +1,13 @@
 package com.alexander_nevsky_temple.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ArticleDto(
     val id: Long,
+    val catalog: ArticleTypeDto,
     val title: String,
     val description: String,
     val dateOrBanner: String,
-    val catalog: ArticleTypeDto,
+    val content: List<String>
 )
--- data/src/main/java/com/alexander_nevsky_temple/data/remote/dto/ArticleTypeDto.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/remote/dto/ArticleTypeDto.kt
@@ -1,5 +1,8 @@
 package com.alexander_nevsky_temple.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ArticleTypeDto(
     val id: Int,
     val name: String
