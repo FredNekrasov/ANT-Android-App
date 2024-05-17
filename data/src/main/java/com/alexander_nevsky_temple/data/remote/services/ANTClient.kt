921c811da775a63a97c91f8249ddd9fc87f6711a
FredNekrasov <frednekrasov49@gmail.com>
2024-05-17 16:03:35 +0300

    creating a HttpClient

---
+++ data/src/main/java/com/alexander_nevsky_temple/data/remote/services/ANTClient.kt
@@ -0,0 +1,31 @@
+package com.alexander_nevsky_temple.data.remote.services
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object ANTClient {
+    val client = HttpClient(OkHttp) {
+        expectSuccess = true
+        install(Logging) {
+            level = LogLevel.ALL
+        }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                }
+            )
+        }
+        defaultRequest {
+            url(DEFAULT_URL)
+        }
+    }
+    private const val DEFAULT_URL = "http://localhost:5115/api/"
+}
