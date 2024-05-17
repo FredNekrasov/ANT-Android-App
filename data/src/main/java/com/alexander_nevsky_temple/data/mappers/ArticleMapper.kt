d900c060c917a551f4758b2b38f68149b524f4a3
FredNekrasov <frednekrasov49@gmail.com>
2024-05-17 16:00:35 +0300

    refactoring mappers

--- data/src/main/java/com/alexander_nevsky_temple/data/mappers/ArticleMapper.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/mappers/ArticleMapper.kt
@@ -1,12 +1,19 @@
 package com.alexander_nevsky_temple.data.mappers
 
+import com.alexander_nevsky_temple.data.local.entities.*
 import com.alexander_nevsky_temple.data.remote.dto.ArticleDto
 import com.alexander_nevsky_temple.domain.model.Article
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
-fun ArticleDto.toModel(content: List<String>) = Article(
-    title,
-    description,
-    dateOrBanner,
-    catalog.toModel(),
-    content
+fun ArticleDto.toEntity() = ArticleEntity(
+    title, description, dateOrBanner, Json.encodeToString(catalog), Json.encodeToString(content), id
+)
+fun ArticleDto.toModel() = Article(
+    title, description, dateOrBanner, catalog.toModel(), content
+)
+fun ArticleEntity.toModel() = Article(
+    title, description, dateOrBanner,
+    Json.decodeFromString<Catalog>(catalog).toModel(),
+    Json.decodeFromString<List<String>>(content)
 )
