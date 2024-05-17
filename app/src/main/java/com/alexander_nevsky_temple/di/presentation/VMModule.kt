250734f0aec15a75c0ce50e5386780b1454d0a17
FredNekrasov <frednekrasov49@gmail.com>
2024-05-17 16:08:36 +0300

    moving DIStrings from the app module to the data module and refactoring vmModule

--- app/src/main/java/com/alexander_nevsky_temple/di/presentation/VMModule.kt
+++ app/src/main/java/com/alexander_nevsky_temple/di/presentation/VMModule.kt
@@ -1,6 +1,6 @@
 package com.alexander_nevsky_temple.di.presentation
 
-import com.alexander_nevsky_temple.di.utils.DIStrings
+import com.alexander_nevsky_temple.data.di.utils.DIStrings
 import com.alexander_nevsky_temple.presentation.viewModels.*
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
@@ -11,6 +11,6 @@ val vmModule = module {
         ArticleVM(get(named(DIStrings.ARTICLE + DIStrings.USE_CASE)))
     }
     viewModel<ArticleTypeVM>(qualifier = named(DIStrings.VIEWMODEL + DIStrings.ARTICLE_TYPE)) {
-        ArticleTypeVM(get(named(DIStrings.ARTICLE + DIStrings.USE_CASE)))
+        ArticleTypeVM(get(named(DIStrings.ARTICLE_TYPE + DIStrings.USE_CASE)))
     }
 }
