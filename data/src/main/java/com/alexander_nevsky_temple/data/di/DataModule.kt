2b410216aea0f9e5a28f91cb25682536bd55b614
FredNekrasov <frednekrasov49@gmail.com>
2024-05-18 11:40:28 +0300

    refactoring the DataModule.kt

--- data/src/main/java/com/alexander_nevsky_temple/data/di/DataModule.kt
+++ data/src/main/java/com/alexander_nevsky_temple/data/di/DataModule.kt
@@ -5,8 +5,7 @@ import com.alexander_nevsky_temple.data.di.utils.DIStrings
 import com.alexander_nevsky_temple.data.local.ANTDb
 import com.alexander_nevsky_temple.data.remote.services.ANTClient
 import com.alexander_nevsky_temple.data.repositories.*
-import com.alexander_nevsky_temple.domain.model.*
-import com.alexander_nevsky_temple.domain.repositories.IRepository
+import com.alexander_nevsky_temple.domain.repositories.IArticleRepository
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
@@ -15,13 +14,7 @@ val dataModule = module {
         Room.databaseBuilder(get(), ANTDb::class.java, ANTDb.DB_NAME).build()
     }
     single(named(DIStrings.CLIENT), createdAtStart = true) { ANTClient.client }
-    single<IRepository<ArticleType>>(qualifier = named(DIStrings.ARTICLE_TYPE + DIStrings.REPOSITORY)) {
-        ArticleTypeRepository(
-            get<ANTDb>(named(DIStrings.DATABASE)).catalogDao(),
-            get(named(DIStrings.CLIENT))
-        )
-    }
-    single<IRepository<Article>>(qualifier = named(DIStrings.ARTICLE + DIStrings.REPOSITORY)) {
+    single<IArticleRepository>(qualifier = named(DIStrings.ARTICLE + DIStrings.REPOSITORY)) {
         ArticleRepository(
             get<ANTDb>(named(DIStrings.DATABASE)).articleDao(),
             get(named(DIStrings.CLIENT))
