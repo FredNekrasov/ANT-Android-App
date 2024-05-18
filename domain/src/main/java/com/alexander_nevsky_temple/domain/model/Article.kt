a00f8743e78a8fc11460bc20fcaaa8e7e0cc32cc
FredNekrasov <frednekrasov49@gmail.com>
2024-05-18 11:36:45 +0300

    refactoring the Article model

--- domain/src/main/java/com/alexander_nevsky_temple/domain/model/Article.kt
+++ domain/src/main/java/com/alexander_nevsky_temple/domain/model/Article.kt
@@ -4,7 +4,7 @@ data class Article(
     val title: String,
     val description: String,
     val date: String,
-    val articleType: ArticleType,
+    val articleType: String,
     val content: List<String>,
     val id: Int? = null
 )
