0100a0271eb2d6c8ce0dc3f2e733e12c046b2257
FredNekrasov <frednekrasov49@gmail.com>
2024-05-19 15:50:11 +0300

    creating a Strings object with string data

---
+++ app/src/main/java/com/alexander_nevsky_temple/ui/customItems/Strings.kt
@@ -0,0 +1,25 @@
+package com.alexander_nevsky_temple.ui.customItems
+
+object Strings {
+    const val MAIN_TITLE = "Храм Александра Невского"
+    const val MENU = "Меню"
+    // error messages
+    const val CONNECTION_ERROR = "Ошибка соединения с сервером"
+    const val NO_INTERNET = "Нет интернета"
+    const val NO_DATA = "Нет данных"
+    const val SERIALIZATION_ERROR = "Сервер отправил некорректные данные"
+    const val UNKNOWN = "Неизвестная ошибка"
+    // screen routes
+    const val MAIN = "Главная страница"
+    const val PARISH_LIFE = "Приходская жизнь"
+    const val SCHEDULE = "Расписание Богослужений"
+    const val SPIRITUAL_TALKS = "Духовные беседы"
+    const val YOUTH_CLUB = "Молодежный клуб"
+    const val PRIESTHOOD = "Священство"
+    const val ADVICES = "Советы священника"
+    const val HISTORY = "История"
+    const val SACRAMENTS = "Требы"
+    const val CONTACTS = "Контакты"
+    const val INFORMATION = "Информация"
+    const val VOLUNTEERISM = "Приходская добровольческая служба"
+}
