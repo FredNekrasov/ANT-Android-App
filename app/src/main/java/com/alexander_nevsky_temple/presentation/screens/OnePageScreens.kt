33b0780a2b99110a2e42aea704a6191eb8844353
FredNekrasov <frednekrasov49@gmail.com>
2024-05-19 21:56:48 +0300

    refactoring the OnePageScreens.kt file

--- app/src/main/java/com/alexander_nevsky_temple/presentation/screens/OnePageScreens.kt
+++ app/src/main/java/com/alexander_nevsky_temple/presentation/screens/OnePageScreens.kt
@@ -4,16 +4,17 @@ import android.widget.TextView
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.net.toUri
 import coil.compose.AsyncImage
 import com.alexander_nevsky_temple.R
-import com.alexander_nevsky_temple.domain.model.Article
-import com.alexander_nevsky_temple.presentation.screens.components.*
+import com.alexander_nevsky_temple.presentation.screens.components.ListItemDetails
 import com.alexander_nevsky_temple.presentation.viewModels.ArticleVM
 import com.alexander_nevsky_temple.ui.customItems.*
 
@@ -22,7 +23,7 @@ fun MainScreen(vm: ArticleVM) {
     Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp)) {
         vm.articlesSF.collectAsState().value.list.forEach {
             if(it.articleType != Strings.MAIN) return@forEach
-            ListItem(it, Modifier.wrapContentSize())
+            ListItemDetails(it, Modifier.fillMaxWidth())
         }
     }
 }
@@ -31,13 +32,13 @@ fun Priesthood(vm: ArticleVM) {
     Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp)) {
         vm.articlesSF.collectAsState().value.list.forEach {
             if(it.articleType != Strings.PRIESTHOOD) return@forEach
-            ListItem(it, Modifier.wrapContentSize())
+            ListItemDetails(it, Modifier.fillMaxWidth())
         }
     }
 }
 @Composable
 fun Schedule(vm: ArticleVM) {
-    Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
+    Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()), Arrangement.Center) {
         vm.articlesSF.collectAsState().value.list.forEach {
             if(it.articleType != Strings.SCHEDULE) return@forEach
             AsyncImage(model = it.title.toUri(), contentDescription = it.title, modifier = Modifier.fillMaxWidth())
@@ -46,17 +47,18 @@ fun Schedule(vm: ArticleVM) {
 }
 @Composable
 fun Sacraments(vm: ArticleVM) {
-    Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp), Arrangement.Top, Alignment.CenterHorizontally) {
+    Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp), Arrangement.Center) {
         vm.articlesSF.collectAsState().value.list.forEach {
             if(it.articleType != Strings.SACRAMENTS) return@forEach
             FredTitle(text = it.title)
-            FredText(text = it.description, modifier = Modifier.wrapContentSize())
+            Spacer(modifier = Modifier.height(8.dp))
+            FredText(text = it.description, modifier = Modifier.fillMaxWidth(), TextAlign.Center)
         }
     }
 }
 @Composable
 fun Contacts(vm: ArticleVM) {
-    Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp), Arrangement.Top, Alignment.CenterHorizontally) {
+    Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp)) {
         val state = vm.articlesSF.collectAsState().value
         state.list.forEach {
             if(it.articleType != Strings.CONTACTS) return@forEach
@@ -68,28 +70,16 @@ fun Contacts(vm: ArticleVM) {
                     setTextColor(resources.getColor(R.color.black, null))
                     textAlignment = TextView.TEXT_ALIGNMENT_CENTER
                 }
-            }, Modifier.wrapContentSize())
+            }, Modifier.fillMaxWidth().align(Alignment.CenterHorizontally))
         }
     }
 }
 @Composable
 fun Volunteerism(vm: ArticleVM) {
-    vm.articlesSF.collectAsState().value.list.forEach {
-        if(it.articleType != Strings.VOLUNTEERISM) return@forEach
-        VolunteerismCard(it)
-    }
-}
-@Composable
-private fun VolunteerismCard(it: Article, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        FredTitle(it.title)
-        Spacer(Modifier.height(8.dp))
-        FredText(it.description)
-        Spacer(Modifier.height(8.dp))
-        ImageSlider(article = it, Modifier.fillMaxWidth())
-        Spacer(Modifier.height(8.dp))
+    Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp)) {
+        vm.articlesSF.collectAsState().value.list.forEach {
+            if(it.articleType != Strings.VOLUNTEERISM) return@forEach
+            ListItemDetails(article = it)
+        }
     }
 }
