1aafe511723b2338f8a64558c19b3b6455cca3d7
FredNekrasov <frednekrasov49@gmail.com>
2024-05-19 16:04:16 +0300

    refactoring the MainActivity

--- app/src/main/java/com/alexander_nevsky_temple/MainActivity.kt
+++ app/src/main/java/com/alexander_nevsky_temple/MainActivity.kt
@@ -1,32 +1,38 @@
 package com.alexander_nevsky_temple
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import com.alexander_nevsky_temple.di.utils.DIStrings
+import androidx.lifecycle.lifecycleScope
 import com.alexander_nevsky_temple.presentation.MainEntryPoint
-import com.alexander_nevsky_temple.presentation.viewModels.ArticleTypeVM
-import com.alexander_nevsky_temple.ui.customItems.*
+import com.alexander_nevsky_temple.ui.customItems.Strings
 import com.alexander_nevsky_temple.ui.theme.AlexanderNevskyTempleTheme
-import org.koin.androidx.viewmodel.ext.android.getViewModel
-import org.koin.core.qualifier.named
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val articleTypeVM = getViewModel<ArticleTypeVM>(qualifier = named(DIStrings.VIEWMODEL + DIStrings.ARTICLE_TYPE))
+        val screenRoutes = listOf(
+            Strings.MAIN,
+            Strings.PARISH_LIFE,
+            Strings.SCHEDULE,
+            Strings.SPIRITUAL_TALKS,
+            Strings.YOUTH_CLUB,
+            Strings.PRIESTHOOD,
+            Strings.ADVICES,
+            Strings.HISTORY,
+            Strings.SACRAMENTS,
+            Strings.CONTACTS,
+            Strings.INFORMATION,
+            Strings.VOLUNTEERISM
+        )
         setContent {
             AlexanderNevskyTempleTheme {
                 Surface(Modifier.fillMaxSize(),color = MaterialTheme.colorScheme.background) {
-                    val state = articleTypeVM.articleTypesSF.collectAsState().value
-                    if(state.status.isError()) displayMessage(state.status.getMessage(), Toast.LENGTH_LONG)
-                    MainEntryPoint(state.list)
+                    MainEntryPoint(screenRoutes)
                 }
             }
         }
