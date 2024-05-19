a9fa8b5eddb84dd1ee0407e6623802d650ad04e1
FredNekrasov <frednekrasov49@gmail.com>
2024-05-19 15:52:32 +0300

    creating a Extensions.kt file with extension methods

---
+++ app/src/main/java/com/alexander_nevsky_temple/ui/customItems/Extensions.kt
@@ -0,0 +1,19 @@
+package com.alexander_nevsky_temple.ui.customItems
+
+import android.content.Context
+import android.widget.Toast
+import com.alexander_nevsky_temple.domain.utils.ConnectionStatus
+import com.alexander_nevsky_temple.domain.utils.ConnectionStatus.*
+
+fun Context.displayMessage(
+    message : String, duration : Int = Toast.LENGTH_SHORT
+) = Toast.makeText(this, message, duration).show()
+fun ConnectionStatus.getMessage() = when(this) {
+    CONNECTION_ERROR -> Strings.CONNECTION_ERROR
+    NO_INTERNET -> Strings.NO_INTERNET
+    NO_DATA -> Strings.NO_DATA
+    SERIALIZATION_ERROR -> Strings.SERIALIZATION_ERROR
+    UNKNOWN -> Strings.UNKNOWN
+    else -> ""
+}
+fun ConnectionStatus.isError() = (this != SUCCESS) && (this != LOADING)
