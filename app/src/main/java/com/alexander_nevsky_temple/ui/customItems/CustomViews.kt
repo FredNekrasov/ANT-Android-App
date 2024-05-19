17e6290673b69945e3115b48247f923f30ea019f
FredNekrasov <frednekrasov49@gmail.com>
2024-05-19 21:52:07 +0300

    refactoring the CustomViews.kt file

--- app/src/main/java/com/alexander_nevsky_temple/ui/customItems/CustomViews.kt
+++ app/src/main/java/com/alexander_nevsky_temple/ui/customItems/CustomViews.kt
@@ -10,19 +10,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil.compose.AsyncImage
 
 @Composable
-fun FredText(text: String, modifier: Modifier = Modifier) {
+fun FredText(text: String, modifier: Modifier = Modifier, textAlign : TextAlign = TextAlign.Justify) {
     Text(
         text,
         modifier,
-        color = MaterialTheme.colorScheme.primary,
         fontFamily = FontFamily.Serif,
-        textAlign = TextAlign.Justify
+        textAlign = textAlign
     )
 }
 @Composable
@@ -30,7 +30,7 @@ fun FredTitle(text: String) {
     Text(
         text,
         Modifier.fillMaxWidth(),
-        fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+        fontWeight = FontWeight.SemiBold,
         fontFamily = FontFamily.Serif,
         textAlign = TextAlign.Center
     )
@@ -63,7 +63,11 @@ fun FredNavigationDrawerItem(text: String, selected: Boolean, onClick: () -> Uni
 }
 @Composable
 fun FredFloatingActionButton(onClick: () -> Unit, icon: ImageVector) {
-    FloatingActionButton(onClick, Modifier.border(1.dp, MaterialTheme.colorScheme.primary, MaterialTheme.shapes.medium)) {
+    FloatingActionButton(
+        onClick = onClick,
+        modifier = Modifier.border(1.dp, MaterialTheme.colorScheme.primary, MaterialTheme.shapes.medium),
+        shape = MaterialTheme.shapes.medium
+    ) {
         Icon(icon, Strings.SCHEDULE)
     }
 }
@@ -71,11 +75,11 @@ fun FredFloatingActionButton(onClick: () -> Unit, icon: ImageVector) {
 fun FredCard(onClick: () -> Unit, uri: String?, title: String, date: String, modifier: Modifier = Modifier) {
     Card(
         onClick,
-        modifier,
+        modifier.border(2.dp, MaterialTheme.colorScheme.primary, MaterialTheme.shapes.medium).padding(8.dp),
         shape = MaterialTheme.shapes.small,
         colors = CardDefaults.outlinedCardColors()
     ) {
-        if(!uri.isNullOrBlank()) AsyncImage(model = uri.toUri(), contentDescription = title, modifier = Modifier.fillMaxWidth())
+        if(!uri.isNullOrBlank()) AsyncImage(model = uri.toUri(), contentDescription = title, modifier = Modifier.fillMaxWidth().height(250.dp))
         Spacer(Modifier.height(4.dp))
         FredText(title, modifier = Modifier.align(Alignment.CenterHorizontally))
         Spacer(Modifier.height(4.dp))
