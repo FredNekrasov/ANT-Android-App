f768d12bb8370626b9fb0847b45783029a123910
FredNekrasov <frednekrasov49@gmail.com>
2024-05-21 23:25:16 +0300

    refactoring the MainEntryPoint and deleting the MainScaffold.kt file

--- app/src/main/java/com/alexander_nevsky_temple/presentation/MainEntryPoint.kt
+++ app/src/main/java/com/alexander_nevsky_temple/presentation/MainEntryPoint.kt
@@ -1,23 +1,29 @@
 package com.alexander_nevsky_temple.presentation
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CalendarMonth
 import androidx.compose.material3.*
 import androidx.compose.material3.DrawerValue.Closed
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
-import com.alexander_nevsky_temple.ui.customItems.FredNavigationDrawerItem
+import androidx.navigation.compose.*
+import com.alexander_nevsky_temple.ui.customItems.*
 import kotlinx.coroutines.launch
 
 @Composable
-fun MainEntryPoint(navItems: List<String>) {
+fun MainEntryPoint(openWebPage : SAction, openSomeApp : SSAction, navItems : List<String> = Strings.SCREEN_ROUTES) {
     val controller = rememberNavController()
     val drawerState = rememberDrawerState(Closed)
     val scope = rememberCoroutineScope()
     var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
+    val navigateTo: (Int, String) -> Unit = { index, route ->
+        controller.navigate(route)
+        selectedItemIndex = index
+        scope.launch { drawerState.close() }
+    }
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
@@ -26,15 +32,26 @@ fun MainEntryPoint(navItems: List<String>) {
                     FredNavigationDrawerItem(
                         text = route,
                         selected = index == selectedItemIndex,
-                        onClick = {
-                            controller.navigate(route)
-                            selectedItemIndex = index
-                            scope.launch { drawerState.close() }
+                        onClick = { 
+                            when(index) {
+                                3 -> openWebPage(Strings.SPIRITUAL_TALKS_URL)
+                                10 -> openWebPage(Strings.INFORMATION_URL)
+                                else -> navigateTo(index, route)
+                            }
                         }
                     )
+                    Spacer(Modifier.height(2.dp))
                 }
             }
         }, drawerState = drawerState) {
-        MainScaffold(controller, { scope.launch { drawerState.open() } }, navItems)
+        val currentRoute = controller.currentBackStackEntryAsState().value?.destination?.route
+        Scaffold(
+            Modifier.fillMaxSize(),
+            topBar = { FredTopAppBar { scope.launch { drawerState.open() } } },
+            floatingActionButton = { if(currentRoute != navItems[2]) FredFloatingActionButton({ navigateTo(2, navItems[2]) }, Icons.Outlined.CalendarMonth) },
+            floatingActionButtonPosition = FabPosition.End
+        ) { padding ->
+            MainNavHost(controller, navItems, openWebPage, openSomeApp, Modifier.fillMaxSize().padding(padding))
+        }
     }
 }
